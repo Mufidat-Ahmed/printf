15379c54704b7525e5c0d0a32c0ebd5090653f0a
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-05-29 06:29:27 -0700

    print

---
+++ print_integer.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * _printf - printf statement that handle %d and %i
+ * @format: character string to be used
+ * @...: various arguments
+ * Return: z
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list m;
+
+	va_start(m, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%' && (*(format + 1) == 'd' || *(format + 1))
+		{
+			int m = va_arg(args, int);
+			int d = 1;
+
+		else if (m < 0)
+		{
+		_putchar('-');
+		m = -m;
+		}
+	if (m / d > 9)
+	{
+
+	}
+	format++;
+	}
+	va_end(m);
+	return (z);
+}
