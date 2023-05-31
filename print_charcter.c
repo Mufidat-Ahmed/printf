4b1dd9ef63d554b28f6a9146c0a7a6df3ce9bf4b
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-05-31 01:42:50 -0700

    prints

--- print_charcter.c
+++ print_charcter.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
+#include <unistd.h>
 
 /**
  * _printf - Prints formatted output to stdout
@@ -54,4 +55,14 @@ int _printf(const char *format, ...)
 	va_end(m);
 	return (z);
 }
-
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
