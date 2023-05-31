6dc73eee239caa28099310c75db5adfd41a5103e
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-05-31 02:52:21 -0700

    print

--- print_binary.c
+++ print_binary.c
@@ -40,14 +40,3 @@ int _print_binary(const char *format, ...)
 	va_end(bin);
 	return (0);
 }
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-        return (write(1, &c, 1));
-}
--- print_integer.c
+++ print_integer.c
@@ -48,14 +48,3 @@ int _print_integer(const char *format, ...)
 	va_end(m);
 	return (0);
 }
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-        return (write(1, &c, 1));
-}
