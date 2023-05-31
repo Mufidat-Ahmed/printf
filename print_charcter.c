25d925155a957f9a9a03c87c0db06c0b1ea57580
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-05-31 01:30:06 -0700

    print

--- print_charcter.c
+++ print_charcter.c
@@ -1,59 +1,57 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
  * _printf - Prints formatted output to stdout
  * @format: Format string
- *
  * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+	va_list m;
+	int z = 0;
 
-    va_start(args, format);
+	va_start(m, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				_putchar('%');
+				z++;
+			}
+			else if (*format == 'c')
+			{
+				int b = va_arg(m, int);
 
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == '\0')
-                break;
-            if (*format == '%')
-            {
-                putchar('%');
-                count++;
-            }
-            else if (*format == 'c')
-            {
-                int c = va_arg(args, int);
-                putchar(c);
-                count++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                int i = 0;
-                while (str[i] != '\0')
-                {
-                    putchar(str[i]);
-                    count++;
-                    i++;
-                }
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
+				_putchar(b);
+				z++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(m, char *);
+				int i = 0;
 
-        format++;
-    }
-
-    va_end(args);
-
-    return count;
+				while (str[i] != '\0')
+				{
+					_putchar(str[i]);
+					z++;
+					i++;
+				}
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			z++;
+		}
+		format++;
+	}
+	va_end(m);
+	return (z);
 }
 
